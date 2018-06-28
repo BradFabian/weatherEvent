93bdf9587fdc4571099ea9c9fff617f50099aec9
Joseph Evangelista <oootep123@gmail.com>
2018-06-28 19:15:23 -0400

    Made algolia input box to know

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -3,18 +3,14 @@ $(document).ready(function(){
         var location= $("#location").val();
         console.log(location);
     })
-
-    var config = {
-        apiKey: "AIzaSyAkkbAUFohZ84LOVG6-YEdvPQYLiM5ZspU",
-        authDomain: "train-scheduler-e510b.firebaseapp.com",
-        databaseURL: "https://train-scheduler-e510b.firebaseio.com",
-        projectId: "train-scheduler-e510b",
-        storageBucket: "train-scheduler-e510b.appspot.com",
-        messagingSenderId: "586386758280"
-      };
-      firebase.initializeApp(config);
-
-      var database = firebase.database();
-
-      database.ref().on()
-})
+    var placesAutocomplete = places({
+        container: document.querySelector('#location'),
+        type: 'city',
+        aroundLatLngViaIP: false,
+        templates: {
+            value: function (suggestion) {
+                return suggestion.name;
+            }
+        }
+    });
+});
