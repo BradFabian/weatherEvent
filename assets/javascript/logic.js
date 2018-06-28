aa17a24b8a8f675df347c44767b06dc07d5d6e74
Joseph Evangelista <oootep123@gmail.com>
2018-06-28 19:20:49 -0400

    algolia input box working

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,16 +1,11 @@
-$(document).ready(function(){
-    $("#submit-button").on("click",function(){
-        var location= $("#location").val();
-        console.log(location);
-    })
-    var placesAutocomplete = places({
-        container: document.querySelector('#location'),
-        type: 'city',
-        aroundLatLngViaIP: false,
-        templates: {
-            value: function (suggestion) {
-                return suggestion.name;
-            }
+var placesAutocomplete = places({
+    container: document.querySelector('#search-inputgit '),
+    type: 'city',
+    aroundLatLngViaIP: false,
+    templates: {
+        value: function (suggestion) {
+            return suggestion.name;
         }
-    });
+    }
 });
+
