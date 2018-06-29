5a34358caf3a8d62e0955a3c0386bfccaedeafed
Joseph Evangelista <oootep123@gmail.com>
2018-06-28 21:11:32 -0400

    added firebase

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -9,115 +9,154 @@ $(document).ready(function () {
     templates: {
       value: function (suggestion) {
         return suggestion.name;
+      }
+    }
+  });
 
-        var city = "Miami";
+  // Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAPiXUM5pySi3Eg0I3rkpNjN_O53VLi2Jw",
+  authDomain: "weatherevent-d8ee7.firebaseapp.com",
+  databaseURL: "https://weatherevent-d8ee7.firebaseio.com",
+  projectId: "weatherevent-d8ee7",
+  storageBucket: "weatherevent-d8ee7.appspot.com",
+  messagingSenderId: "444172683620"
+};
+firebase.initializeApp(config);
 
-        // Here we construct our URL
-        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=ff67ab04503ce887f4d15b122c1904ed";
-        // Write code between the dashes below to hit the queryURL with $ajax, then take the response data
-        // and display it in the div with an id of movie-view
-        // ------YOUR CODE GOES IN THESE DASHES. DO NOT MANUALLY EDIT THE HTML ABOVE.
-        $.ajax({
-          url: queryURL,
-          method: "GET"
-        }).then(function (response) {
-          // Log the resulting object
-          console.log(response);
+var database = firebase.database();
 
-          console.log("ready!");
-        });
+database.ref().on("value",function(snapshot){
+  var recentSearches = snapshot.val().events;
+  console.log("is it working")
 
-        //API Working Code//
-        $("#btn").on("click", function (event) {
+  $("#recentSearches> Tbody").append("<tr>"+ recentSearches+ "</tr>");
 
-          // event.preventDefault() can be used to prevent an event's default behavior.
-          // Here, it prevents the submit button from trying to submit a form when clicked
-          event.preventDefault();
+});
 
-          // Here we grab the text from the input box
-          var city = $("#cityInput").val().trim();
+  $("#search-input").on("click", function(event){
+    event.preventDefault();
+    var recentSearches = $("#search-input").val().trim();
 
-          // Here we construct our URL
-          var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=ff67ab04503ce887f4d15b122c1904ed";
+    var newSearch = {
+      events:searched
+    }
 
+    database.ref().push(newSearch)
 
+    $("#search-input").val("");
 
-          $.ajax({
-            url: queryURL,
-            method: "GET"
-          }).then(function (response) {
-            // Log the resulting object
-            console.log(response);
-
-            var iconcode = response.weather[0].icon;
-            var iconurl = 'http://openweathermap.org/img/w/' + iconcode + '.png';
-
-            // Transfer content to HTML
-            $(".city").html("<h1>" + response.name + " Weather Details</h1>");
-            $('.icon').html("<img id='wicon' src=" + iconurl + " alt='Weather icon'>");
-            $(".rain").text("Rain:" + response.weather[0].description);
-            $(".wind").text("Wind Speed:" + response.wind.speed);
-            $(".humidity").text("Humidity:" + response.main.humidity);
-            $(".temp").text("Temperature (F) " + response.main.temp);
-
-            // Log the data in the console as well
-            console.log("Wind Speed: " + response.wind.speed);
-            console.log("Humidity: " + response.main.humidity);
-            console.log("Temperature (F): " + response.main.temp);
+  })
 
 
-          });
+  var city = "Miami";
 
-          city
+  // Here we construct our URL
+  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=ff67ab04503ce887f4d15b122c1904ed";
+  // Write code between the dashes below to hit the queryURL with $ajax, then take the response data
+  // and display it in the div with an id of movie-view
+  // ------YOUR CODE GOES IN THESE DASHES. DO NOT MANUALLY EDIT THE HTML ABOVE.
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
+    // Log the resulting object
+    console.log(response);
 
-          // Event City Query//
-          var eventURL = "https://api.eventful.com/json/events/search?l=" + city + "&app_key=8qj2VzB2Xt2BG7Zh";
+    console.log("ready!");
+  });
 
+  //API Working Code//
+  $("#btn").on("click", function (event) {
 
-          $.ajax({
-            url: eventURL,
-            method: "GET",
-            dataType: 'jsonp'
-          }).then(function (response) {
-            // Log the resulting object
+    // event.preventDefault() can be used to prevent an event's default behavior.
+    // Here, it prevents the submit button from trying to submit a form when clicked
+    event.preventDefault();
+
+    // Here we grab the text from the input box
+    var city = $("#cityInput").val().trim();
+
+    // Here we construct our URL
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=ff67ab04503ce887f4d15b122c1904ed";
 
 
 
-            console.log(response.events.event[0])
-            console.log(response.events.event[1])
-            console.log(response.events.event[2])
-            console.log(response.events.event[3])
-            console.log(response.events.event[4])
-            console.log(response.events.event[5])
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function (response) {
+      // Log the resulting object
+      console.log(response);
 
-            var results = response.events.event
+      var iconcode = response.weather[0].icon;
+      var iconurl = 'http://openweathermap.org/img/w/' + iconcode + '.png';
+
+      // Transfer content to HTML
+      $(".city").html("<h1>" + response.name + " Weather Details</h1>");
+      $('.icon').html("<img id='wicon' src=" + iconurl + " alt='Weather icon'>");
+      $(".rain").text("Rain:" + response.weather[0].description);
+      $(".wind").text("Wind Speed:" + response.wind.speed);
+      $(".humidity").text("Humidity:" + response.main.humidity);
+      $(".temp").text("Temperature (F) " + response.main.temp);
+
+      // Log the data in the console as well
+      console.log("Wind Speed: " + response.wind.speed);
+      console.log("Humidity: " + response.main.humidity);
+      console.log("Temperature (F): " + response.main.temp);
+
+
+    });
+
+    city
+
+    // Event City Query//
+    var eventURL = "https://api.eventful.com/json/events/search?l=" + city + "&app_key=8qj2VzB2Xt2BG7Zh";
+
+
+    $.ajax({
+      url: eventURL,
+      method: "GET",
+      dataType: 'jsonp'
+    }).then(function (response) {
+      // Log the resulting object
+
+
+
+      console.log(response.events.event[0])
+      console.log(response.events.event[1])
+      console.log(response.events.event[2])
+      console.log(response.events.event[3])
+      console.log(response.events.event[4])
+      console.log(response.events.event[5])
+
+      var results = response.events.event
 
 
 
 
-            // Looping over every result item
-            for (var i = 0; i < results.length; i++) {
+      // Looping over every result item
+      for (var i = 0; i < results.length; i++) {
 
-              // Obtain a reference to the tbody element in the DOM
-              var tbody = $("#eventSchedule");
-              // Create and save a reference to new empty table row
-              var tr = $("<tr></tr>");
-              // Create and save references to 6 td elements 
-              var city = '<td>' + response.events.event[i].city_name + '</td>';
-              var date = '<td>' + response.events.event[i].start_time + '</td>';
-              var venueName = '<td>' + response.events.event[i].venue_name + '</td>';
-              var description = '<td>' + response.events.event[i].description + '</td>';
-              var address = '<td>' + response.events.event[i].venue_address + '</td>';
-              var contact = '<td>' + response.events.event[i].venue_url + '</td>';
-              console.log(city);
-              console.log(date);
-              console.log(venueName);
-              console.log(description);
-              console.log(address);
-              console.log(contact);
+        // Obtain a reference to the tbody element in the DOM
+        var tbody = $("#eventSchedule");
+        // Create and save a reference to new empty table row
+        var tr = $("<tr></tr>");
+        // Create and save references to 6 td elements 
+        var city = '<td>' + response.events.event[i].city_name + '</td>';
+        var date = '<td>' + response.events.event[i].start_time + '</td>';
+        var venueName = '<td>' + response.events.event[i].venue_name + '</td>';
+        var description = '<td>' + response.events.event[i].description + '</td>';
+        var address = '<td>' + response.events.event[i].venue_address + '</td>';
+        var contact = '<td>' + response.events.event[i].venue_url + '</td>';
+        console.log(city);
+        console.log(date);
+        console.log(venueName);
+        console.log(description);
+        console.log(address);
+        console.log(contact);
 
-            }
-          });
-        });
+      }
+    });
+  });
 
-      });
+});
